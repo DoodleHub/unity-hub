b41e506ecbc5a52a2fef5f2e8964ecb87d487384
Chao Chen <chaochen@live.com>
2024-07-21 18:35:27 -0400

    chore: add shadcn button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
+import { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 
 import { cn } from '@/lib/utils';
 
-import '@/styles/globals.css';
+import './globals.css';
 
 const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
 });
+
+export const metadata: Metadata = {
+  title: 'UnityHub',
+  description: 'Your go-to collaborative editor',
+};
 
 export default function RootLayout({
   children,
@@ -18,10 +24,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head />
       <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
-        )}
+        className={cn('min-h-screen font-sans antialiased', fontSans.variable)}
       >
         {children}
       </body>
